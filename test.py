584abfa0c6e449b2675640d1ea5b95dfa2eb41b1
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-01-24 08:20:12 +0200

    multi window rendering with glnext

---
+++ test.py
@@ -0,0 +1,18 @@
+import glnext
+import imageio
+import mymodule
+
+windows = mymodule.init()
+instance = glnext.instance()
+
+image = instance.image((640, 360), mode='output')
+buffer = instance.staging_buffer([[image]])
+
+for wnd in windows:
+    instance.surface(wnd, image)
+
+reader = imageio.get_reader('assets/bunny.mp4')
+
+for im in reader:
+    buffer.write(glnext.rgba(im, 'rgb'))
+    instance.render()
